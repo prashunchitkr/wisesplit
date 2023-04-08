e2e63d9db19f27d005f2cc0d63fc02ab092cbfd5
Prashun Chitrakar <prashunchi@gmail.com>
2023-04-08 11:26:56 +0545

    Update index unit test

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 import App from "../App";
 
 describe("index", () => {
-  test("Should render Hello World", () => {
+  test("Should render Hello World", async () => {
     render(<App />);
-    expect(screen.getByText("Hello World")).toBeInTheDocument();
+    expect(await screen.findByText("Person")).toBeInTheDocument();
+    expect(await screen.findByText("Amount")).toBeInTheDocument();
+    expect(await screen.findAllByText("Add")).toHaveLength(1);
   });
 });
