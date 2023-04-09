5857b678665300efb2e1196b5ce1b6bf8b52f75a
Prashun Chitrakar <prashunchi@gmail.com>
2023-04-09 12:35:49 +0545

    add react-hook-form

--- src/components/Expense/ExpenseForm.component.tsx
+++ src/components/Expense/ExpenseForm.component.tsx
@@ -1,55 +1,99 @@
-import { IExpense } from "@/core";
 import { useExpenseStore } from "@/store";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface IPaymentForm {
+  person: string;
+  amount: number;
+}
 
 export const ExpenseForm = () => {
-  const { addExpense } = useExpenseStore();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset: resetForm,
+  } = useForm<IPaymentForm>();
+  const { addExpense, reset } = useExpenseStore();
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const formData = new FormData(form);
-
-    if (!formData.get("person") || !formData.get("amount")) return;
-
-    const expense: IExpense = {
-      person: formData.get("person") as string,
-      amount: Number(formData.get("amount")),
-    };
-    addExpense(expense);
-    form.reset();
+  const submit: SubmitHandler<IPaymentForm> = (data) => {
+    addExpense({
+      id: crypto.randomUUID(),
+      person: data.person,
+      amount: data.amount,
+    });
+    resetForm();
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-control w-full">
-          <label className="label" htmlFor="person">
-            <span className="label-text">Person</span>
+    <form onSubmit={handleSubmit(submit)}>
+      <div className="form-control w-full">
+        <label className="label" htmlFor="person">
+          <span className="label-text">Person</span>
+        </label>
+        <input
+          className="input input-bordered input-accent w-full"
+          id="person"
+          {...register("person", {
+            required: {
+              value: true,
+              message: "Person name is required",
+            },
+            minLength: {
+              value: 1,
+              message: "Minimum length is 1",
+            },
+          })}
+        />
+        {errors.person && (
+          <label className="label">
+            <span className="label-text-alt text-red-500">
+              {errors.person.message}
+            </span>
           </label>
-          <input
-            className="input input-bordered input-accent w-full"
-            type="text"
-            name="person"
-            id="person"
-          />
-        </div>
+        )}
+      </div>
 
-        <div className="form-control w-full">
-          <label className="label" htmlFor="amount">
-            <span className="label-text">Amount</span>
-          </label>
-          <input
-            className="input input-bordered input-accent w-full"
-            type="number"
-            name="amount"
-            id="amount"
-          />
-        </div>
+      <div className="form-control w-full">
+        <label className="label" htmlFor="amount">
+          <span className="label-text">Amount</span>
+        </label>
+        <input
+          className="input input-bordered input-accent w-full"
+          id="amount"
+          type="number"
+          min={0}
+          {...register("amount", {
+            valueAsNumber: true,
+            required: {
+              value: true,
+              message: "Amount is required",
+            },
+            min: {
+              value: 0,
+              message: "Minimum amount is 0",
+            },
+          })}
+        />
+      </div>
+      {errors.amount && (
+        <label className="label">
+          <span className="label-text-alt text-red-500">
+            {errors.amount.message}
+          </span>
+        </label>
+      )}
 
-        <button className="btn btn-primary w-full" type="submit">
-          Add
-        </button>
-      </form>
-    </div>
+      <button className="btn btn-primary w-full mt-2" type="submit">
+        Add
+      </button>
+
+      <button
+        className="btn btn-warning w-full mt-2"
+        type="button"
+        onClick={reset}
+      >
+        Reset Expenses
+      </button>
+    </form>
   );
 };
